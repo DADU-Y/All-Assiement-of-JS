1e7641fd52844d637e44ccf82976fdad9b4a0289
DADU-Y <daduyameen2@gmail.com>
2025-02-12 12:27:29 +0500

    Assiement of JS

---
+++ (Assiement no 1)/script.js
@@ -0,0 +1,110 @@
+// Chapter 1 (Alerts)
+// QUESTION NO 1?
+// 1. Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number V. Password
+
+// ANSWER
+
+// FIRST NAME
+
+alert("YAMEEN")
+
+// LAST NAME
+
+alert("DADU")
+
+// EMAIL
+
+alert("yameen54@gmail.com")
+
+// PHONE NUMBER
+
+alert(9213740139837)
+
+// PHONE NUMBER
+
+alert("03128714639")
+
+// PASSWORD
+
+alert("dadu1234!@#$")
+
+// QUESTION NO 2?
+
+// 2. Correct this statement alert"Your learning JavaScript" ;
+
+// ANSWER
+
+alert("your learning JavaScript")
+
+// QUESTION NO 3?
+
+// 3. Code an alert statement displaying any message you like.
+
+// ANSWER
+
+alert("welcome user")
+
+// Chapter 2 (Variables for string)
+
+// QUESTION 1?
+
+// 1. Declare any variable in the camel Case format.
+
+// ANSWER
+
+var myName = "Yameen";
+
+var thisIsBigNumber = 1000;
+
+// QUESTION 2?
+
+// 2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+
+// ANSWER
+
+var va = 56;
+
+alert(va)
+
+// QUESTION 3?
+
+// 3. Declare the variable teamName and Alert your Team name.
+
+// ANSWER
+
+var daduTeam = "best web"
+
+alert(daduTeam)
+
+// QUESTION 4?
+
+// 4. This statement has already been coded. var bestMan = "carlie"; Assign the variable a new string.
+
+// ANSWER
+
+var bestMan = "Charlie";
+
+var bestMan = 40;
+
+document.write(bestMan)
+
+// Chapter 3 (Variables for numbers)
+
+// QUESTION 1,2
+
+// 1. Declare a variable “caseQty”
+
+// 2. Assign to the variable caseQty, which has already been declared,the value 144.
+
+var caseQty = 144
+
+document.write(caseQty)
+
+
+// sir baki kai question samj nahi a rahai kai app kya kah rahai hai   
+
+
